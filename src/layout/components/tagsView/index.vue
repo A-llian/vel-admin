60aad54be312d2829354eff08ae9513fc5ac3be4
anhang <15231159930@163.com>
2022-06-28 12:06:50 +0800

    标签页

--- src/layout/components/tagsView/index.vue
+++ src/layout/components/tagsView/index.vue
@@ -174,26 +174,26 @@ onMounted(() => {
       }
     }
   }
-  .contextmenu {
-    margin: 0;
-    background: #fff;
-    z-index: 3000;
-    position: absolute;
-    list-style-type: none;
-    padding: 5px 0;
-    border-radius: 4px;
-    font-size: 12px;
-    font-weight: 400;
-    color: #333;
-    box-shadow: 2px 2px 3px 0 rgba(0, 0, 0, .3);
-    li {
-      margin: 0;
-      padding: 7px 16px;
-      cursor: pointer;
-      &:hover {
-        background: #eee;
-      }
-    }
-  }
+  // .contextmenu {
+  //   margin: 0;
+  //   background: #fff;
+  //   z-index: 3000;
+  //   position: absolute;
+  //   list-style-type: none;
+  //   padding: 5px 0;
+  //   border-radius: 4px;
+  //   font-size: 12px;
+  //   font-weight: 400;
+  //   color: #333;
+  //   box-shadow: 2px 2px 3px 0 rgba(0, 0, 0, .3);
+  //   li {
+  //     margin: 0;
+  //     padding: 7px 16px;
+  //     cursor: pointer;
+  //     &:hover {
+  //       background: #eee;
+  //     }
+  //   }
+  // }
 }
 </style>
